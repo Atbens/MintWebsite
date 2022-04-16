28bcde25ed0e09d8e5c0c19a4a5d747e09bd5617
Atbens <2650791349@qq.com>
2022-04-16 23:18:47 +0800

    all

---
+++ components/Roadmap/Roadmap.js
@@ -0,0 +1,105 @@
+
+
+
+
+function Roadmap() {
+    return (
+     <div id="roadmap" className="rbackground">
+       <div className=" flex flex-col items-center justify-center sm:px-0 px-6 z-20 pb-32">
+        <div className="md:py-30 py-20">
+          <h1
+            role="heading"
+            className="xl:text-6xl md:text-6xl text-xl font-mono font-semibold leading-10 text-white"
+          >
+            Roadmap 
+           
+          </h1>
+           </div>
+
+           <div className="con fontstyle1" > 
+           <h5>We’re in this for the long haul.</h5> 
+           <h6>We’ve set up a timeline for ourselves and will work on realizing the stated goal in order.</h6>
+           </div>
+           
+       <div className="time-axis clearfix">
+         
+         <div className="left">
+         <span className="dot1"></span>
+          <span className="dot"></span>
+          <div className="con fontstyle1 "> <h3 className="xl:text-2xl"><span>“</span>Initial Construction<span>”</span></h3>
+           <h4>We are working hard to build the community, and give 371 whitelist free minting places to
+             those who initially participated in community building, 740 presalelist places to 
+             those who participated in community building early.</h4> </div>
+          
+         </div>
+
+         <div className="right">
+         <span className="dot"></span>
+         <div className="con fontstyle1"><h3 className="xl:text-2xl"><span>“</span>Public Sale<span>”</span></h3>
+         <h4>A total of 10,000 Emoji NFTs are available for public sale.
+            Each Emoji NFT costs 0.015ETH. Let's go!</h4>
+         </div>
+         </div>
+
+         <div className="left">
+         <span className="dot"></span>        
+          <div className="con fontstyle1"><h3 className="xl:text-2xl"><span>“</span>The Bar<span>”</span></h3>
+          <h4>Building an exclusive bar for the club, and those who hold Emoji NFTs will get membership. This bar can be used by members to chat, socialize and design new emojis.
+          We will create a leaderboard for Emoji based on the number of times they are used in the club. Community airdrops are distributed to NFT holders every month according to the leaderboard.</h4>
+          </div>
+         </div>
+
+         <div className="right">
+         <span className="dot"></span>
+          <div className="con fontstyle1"><h3 className="xl:text-2xl"><span>“</span>Token<span>”</span></h3>
+          <h4>Community governance tokens will be introduced,
+             and who holds Emoji NFT will be able to receive 10,000 token airdrops.
+             Simultaneously, Token staking will be introduced which will allow people to earn in either short or long term.
+             </h4>
+          </div>
+         </div>
+
+         <div className="left">
+         <span className="dot"></span>        
+          <div className="con fontstyle1"><h3 className="xl:text-2xl"><span>“</span>NFTs For Game<span>”</span></h3>
+          <h4>Character NFTs that launch our pirate gambling game.
+             These character NFTs will be divided into regular and rare versions.
+              The total supply of rare NFTs are 10,000.
+               The normal version of NFT will be released in batches and in unlimited quantities
+                at a low price, in order to lower the game threshold and give everyone a fair chance to join our game.
+                 As long as you hold the property NFT, you can enter the game. Members of our club will have a whitelist of character NFTs, and members with rare Emoji NFTs will have a whitelist of rare character NFTs.</h4>
+          </div>
+         </div>
+
+         <div className="right">
+         <span className="dot"></span>        
+          <div className="con fontstyle1"><h3 className="xl:text-2xl"><span>“</span>The V2 Roadmap<span>”</span></h3>
+          <h4>............</h4>
+          </div>
+         </div>
+
+         <div className="left">
+         <span className="dot"></span>        
+          <div className="con fontstyle1"><h3 className="xl:text-2xl"><span>“</span>The Game Dice<span>”</span></h3>
+          <h4>............</h4>
+          </div>
+         </div>
+
+         <div className="right">
+         <span className="dotl"></span>
+         <span className="dot"></span>        
+          <div className="con fontstyle1"><h3 className="xl:text-2xl"><span>“</span>Emoji Evolution<span>”</span></h3>
+          <h4>............</h4>
+          </div>
+         </div>
+        
+
+
+       </div>
+     </div>
+     </div>
+    );
+};
+
+export default Roadmap;
+
